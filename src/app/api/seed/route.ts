c7b2718277c7b9a24be64653c13a68658773e5d6
hugobepa <hugo_bcn@hotmail.com>
2025-11-07 12:13:14 +0100

    install_prisma_correcto_1212_0711

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,27 @@
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+/*
+  await prisma.todo.deleteMany(); // delete * from todo
+
+  await prisma.todo.createMany({
+    data: [
+      { description: 'Piedra del alma', complete: true },
+      { description: 'Piedra del poder' },
+      { description: 'Piedra del tiempo' },
+      { description: 'Piedra del espacio' },
+      { description: 'Piedra del realidad' },
+    ]
+  })
+ 
+  */
+ 
+  const todo = await prisma.todo.create({
+    data: {description: 'piedra del alma'}
+
+  })
+
+  console.log(todo);
+  return NextResponse.json({ message: 'Seed Executed' });
+}
